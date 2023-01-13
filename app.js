7d123dd092a8e14f09dc739bc0fa6c5eaf710cc9
Mehul Gandhi <gandhi@berkeley.edu>
2023-01-12 23:11:41 -0800

    Add CSS to clicking the buttons and fix decimal logic along with paranthesis

--- app.js
+++ app.js
@@ -7,28 +7,15 @@ var curr_decimal = false;
 /** The valid key presses that can be used. */
 var valid_keys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                  "/", "*", "x", "-", "+", "=", "%",
-                  "(", ")", "c", ".", "backspace", "enter"];
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, a, b) {
-    return operator(a, b);
-}
-
-/** Simplifies the expression in input. */
+                  "(", ")", "c", ".", "backspace", "enter"
+                ];
+            
+/** Simplifies the expression stored in s (input).
+ * evaluate has an inner function calc, which takes in an
+ * index. calc has inner function update sign that updates
+ * the stack based on the operator (sign) being applied. If
+ * there are paranthesis in s (input), calc recursively simplifies the
+ * paranthesis expressions. */
 function evaluate(s) {
     var calc = function(i) {
         var num = 0;
@@ -36,6 +23,8 @@ function evaluate(s) {
         var sign = "+";
         var char;
         var decimal = "";
+        /** Returns false if the function divides by zero. Else true.
+         * Updates the stack that stores the evaluation of the current expression. */
         function update(sign) {
             if (sign == "+") {
                 stack.push(num);
@@ -47,15 +36,17 @@ function evaluate(s) {
                 if (num != 0) {
                     stack[stack.length - 1] = stack[stack.length - 1] / num;
                 } else {
-                    alert("Error! Division By Zero!")
+                    alert("Error! Division By Zero!") //will reset the calculator back to zero
                     stack[stack.length - 1] = 0;
+                    return false;
                 }
             } 
+            return true;
         }
         while (i < s.length) {
             char = s[i];
             /** This is here to have UI show the x instead of asterisk. */
-            if (char == "×") {
+            if (char == "×" || char == "x") {
                 char = "*";
             }
 
@@ -65,6 +56,7 @@ function evaluate(s) {
             } else if (char == ".") {
                 i+= 1;
                 char = s[i];
+                /** Adds the mantissa part of the floating point number. */
                 while ((i < s.length && /^-?\d+$/.test(char)) || char === " ") {
                     if (char == " ") {
                         i += 1;
@@ -75,13 +67,16 @@ function evaluate(s) {
                         char = s[i];
                     }
                 }
-                num = parseFloat(parseFloat(num + "." + decimal).toFixed(10));
+                num = parseFloat(parseFloat(num + "." + decimal).toFixed(10)); //new floating point number
                 decimal = "";
                 i -= 1;
-            } else if (/^-?\d+$/.test(char)) {
+            } else if (/^-?\d+$/.test(char)) { //if character is a digit
                 num = num * 10 + parseInt(char);
             } else if (["+", "-", "/", "*"].includes(char)) {
-                update(sign);
+                /** Manages the division by zero error. */
+                if (!update(sign)) {
+                    return 0;
+                }
                 sign = char;
                 num = 0;
             } else if (char == "%") {
@@ -99,20 +94,48 @@ function evaluate(s) {
             i++;
         }
         update(sign);
-        return stack.reduce((acc, val) => acc + val, 0);
+        return stack.reduce((acc, val) => acc + val, 0); //sum the stack
     }
-    return calc(0).toString();
+    /** Check if result is a floating point. If so, leave
+     * curr_decimal flag to true. */
+    let result = calc(0).toString();
+    if (!(result % 1 === 0)) { 
+        curr_decimal = true; 
+    } 
+    return result;
 };
+
+/** Ensures there are no empty brackets and that the number of closed paranthesis
+ * does not exceed the number of open paranthesis. Append closed paranthesis if 
+ * the number of open paranthesis is greater than the number of closed paranthesis. */
+function validate() {
+    var operators = ["×", "/", "+", "-", "x", "*"];
+    if (input.includes("()") || operators.includes(input.slice(-1))) {
+        return false;
+    }
+    let num_open_paranthesis = input.split("(").length - 1;
+    let num_close_paranthesis = input.split(")").length - 1;
+    while (num_open_paranthesis > num_close_paranthesis) {
+        input = input + ")";
+        num_close_paranthesis += 1;
+    }
+    return num_open_paranthesis == num_close_paranthesis;
+}
 
 /**Updates the calculator display. Evalutes the expression if the equals
 * button is pressed. */
 function updateDisplay(entry) {
-    var operators = ["×", "/", "+", "-", "%"];
+    var operators = ["×", "/", "+", "-", "%", "x", "*"];
 
     if (entry == "=" || entry == "enter") {
+        if (!validate()) {
+            return;
+        }
         input = evaluate(input);
-        curr_decimal = false;
     } else if (entry == "backspace") {
+        if (input.slice(-1) == ".") {
+            curr_decimal = false;
+        }
         input = input.slice(0, - 1);
         if (input.length == 0) {
             input = "0";
@@ -121,33 +144,44 @@ function updateDisplay(entry) {
         return;
     } else if (entry == "c") {
         input = "0";
-    } else if (input.slice(-1) == 0 && entry == "." && !curr_decimal) {
+    } else if (input.length == 1 && input == 0 && entry == "." && !curr_decimal) {
         input = input + "."
         curr_decimal = true;
-    } else if (input == 0 && input.slice(-1) != ".") {
-        input = entry;
+    } else if (input.length == 1  && input != "(" && input == 0) {
+        if (entry == "-" || !operators.includes(entry)) {
+            input = entry;
+        }
     } else if (operators.includes(entry) && operators.includes(input.slice(-1))) {
             return; /** Only one operator can be used at a time. One operator cannot be followed by another operator. */
     } else if (entry == "." && curr_decimal) {
             return;
+    } else if (entry == ".") {
+        input += entry;
+        curr_decimal = true;
+    } else if (entry == "(" && !operators.includes(input.slice(-1))) {
+        input = input + "x" + entry;
     } else {
         if (operators.includes(entry) && entry != "%") {
             curr_decimal = false;
         }
         input = input + `${entry}`;
     }
-
     document.getElementById("input").innerHTML = input;
 }
 
-
+/** An event listener to support button click input. */
 var userSelection = document.getElementsByClassName("entry");
 for (let i = 0; i < userSelection.length; i++) {
     userSelection[i].addEventListener("click", () => (updateDisplay(userSelection[i].id)));
 }
+
+/** An event listener to support keyboard input. Must be a valid key. */
 document.addEventListener('keydown', (e) => {
     if (valid_keys.includes(e.key.toLowerCase())) {
-        updateDisplay(e.key.toLowerCase());
+        if (e.key == "*") {
+            updateDisplay("×");
+        } else {
+            updateDisplay(e.key.toLowerCase());
+        }
     }
-}
-);
+});
